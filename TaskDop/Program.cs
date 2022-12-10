22204de88001523734cb16f00eeb0ab1d0b6cea7
Chub <ychubykin@gmail.com>
2022-12-10 16:41:30 +0300

    homework done

--- TaskDop/Program.cs
+++
@@ -1,4 +0,0 @@
-﻿Гипотеза Гольдбаха
-(https://acmp.ru/asp/do/index.asp?main=task&id_course=1&id_section=6&id_topic=117&id_problem=723)
-Статистика(массивы)
-(https://acmp.ru/asp/do/index.asp?main=task&id_course=1&id_section=5&id_topic=114&id_problem=703)
